8ddc8542cce4bb775979cdbb06d337c8cc127bbf
domingos alexandre <daxjunior36@gmail.com>
2022-02-07 21:17:34 -0300

    termino do requisito 08

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,8 +1,12 @@
 const fetchProducts = async (item) => {
+  try {
   const url = `https://api.mercadolibre.com/sites/MLB/search?q=${item}`;
   const retorno = await fetch(url);
   const data = await retorno.json();
   return data;
+  } catch (error) {
+  return (error);
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -10,5 +10,17 @@ it ('Execute a função fetchProducts com o argumento "computador" e teste se fe
   await fetchProducts('computador')
   expect(fetch).toHaveBeenCalled();
 }) 
+it ('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', async () => {
+  await fetchProducts('computador')
+  expect(fetch).toHaveBeenCalledWith("https://api.mercadolibre.com/sites/MLB/search?q=computador");
+  
+})
+it ('Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo', async () => {
+  expect(await fetchProducts('computador')).toMatchObject(computadorSearch);
+  
+})
+it ('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url. Dica: Lembre-se de usar o new Error("mensagem esperada aqui") para comparar com o objeto retornado da API.', async () => {
+expect(await fetchProducts()).toEqual(new Error('You must provide an url'));
+})
 });
 
