b1b2a1f8ae5839a6b06fc582728e3be0af418374
domingos alexandre <daxjunior36@gmail.com>
2022-02-09 21:25:38 -0300

    requisito 06

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,5 +1,8 @@
 const saveCartItems = () => {
-  // seu código aqui
+  const olPai = document.getElementsByClassName('cart__items')[0];
+  const dados = olPai.innerHTML;
+ localStorage.setItem('cartItems', dados);
+ console.log(dados);
 };
 
 if (typeof module !== 'undefined') {
